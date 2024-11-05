05f82d4baecf7b8ff6860fffcd107221caf69fb5
Gavin-Hu-ICD2O1-01 <gavin.hu@student.tdsb.on.ca>
2024-11-05 21:44:53 +0000

    1.2

--- PP1_2.py
+++ PP1_2.py
@@ -3,45 +3,43 @@
 #Write Assignment code here
 
 def q1():
-name = Alex
-print (name)
+    name = "Alex"
+    print (name)    
 
 def q2():
-three = 3
-five = 5
-two = 2
-print (three + five + two)
+    three = 3
+    five = 5
+    two = 2
+    print (three + five + two)
 
 def q3():
-twentyone = 21
-four = 4
-print (21//4)
+    twentyone = 21
+    four = 4
+    print (twentyone/four)
 
 def q4():
-6.3 == float()
-3.0 == float()
-print (6.3/3.0)
+    print (6.3/3.0)
 
 def q5():
-variable = ("Hello World")
-print (variable)
-variable = ("Goodbye World")
-print (variable)
+    variable = ("Hello World")
+    print (variable)
+    variable = ("Goodbye World")
+    print (variable)
 
 def q6():
-variable = True
-print (variable)
+    variable = True
+    print (variable)
 
 
 def q7():
-seven = 7
-two = 2
-print (7%2)
+    seven = 7
+    two = 2
+    print (7%2)
 
-#q1()
-#q2()
-#q3()
-#q4()
-#q5()
-#q6()
-#q7()
+q1()
+q2()
+q3()
+q4()
+q5()
+q6()
+q7()
